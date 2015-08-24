7b8c4234a559a93dcf5eda8e1ebe39322cb3e14e
Md. Nazmul Hassan <nayemDevs@gmail.com>
2015-08-24 12:26:21 +0600

    Update remove-option.php

--- remove-option.php
+++ remove-option.php
@@ -1,9 +1,9 @@
 <?php 
 
 /*
-Plugin Name: Dokan Menu Hider 
-Plugin URI: https://github.com/nayemDevs/dokan-menu-hider
-Description: Remove seller dashboard menu easily in Dokan plugin
+Plugin Name: Dokan tab remover
+Plugin URI: https://github.com/nayemDevs/Dokan-tab-remover
+Description: Remove seller dashboard menu/tab easily - Dokan
 Author: Nayem
 Version: 0.1
 Author URI: http://twitter.com/nayemDevs
@@ -17,103 +17,101 @@ TextDomain: dokan
 add_filter( 'dokan_settings_fields', 'wlr_tab_remover' );
 
 
-	function wlr_tab_remover($settings_fields){
+function wlr_tab_remover($settings_fields){
 
-		 $settings_fields ['dokan_selling'] = array(
-									'remove_tab' => array(
-				                    'name'    => 'remove_tab',
-				                    'label'   => __( 'Hide Dashboard Menu/Tab For Seller', 'dokan' ),
-				                    'desc'    => __( 'Select the dashboard menu to hide from seller', 'dokan' ),
-				                    'type'    => 'select',
-				                    'options' => array(
-				                    			''		   =>'--Select--',	
-							                    'products' => 'Products',
-							                    'orders'   => 'Orders',
-							                    'withdraw' => 'Withdraw',
-											    'coupons'  => 'Coupons', 
-											    'reviews'  => 'Reviews',
-											    'reports'  => 'Reports',
-											    'payment'  => 'Payment',
-											    'shipping' => 'Shipping',
-											    'social'   => 'Social'
+	$settings_fields ['dokan_selling'] = array(
+		'remove_tab' => array(
+			'name'    => 'remove_tab',
+			'label'   => __( 'Hide Seller Dashboard Menu', 'dokan' ),
+			'desc'    => __( 'Select the dashboard menu to hide from seller', 'dokan' ),
+			'type'    => 'select',
+			'options' => array(
+				''		   =>'--Select--',	
+				'products' => 'Products',
+				'orders'   => 'Orders',
+				'withdraw' => 'Withdraw',
+				'coupons'  => 'Coupons', 
+				'reviews'  => 'Reviews',
+				'reports'  => 'Reports',
+				'payment'  => 'Payment',
+				'shipping' => 'Shipping',
+				'social'   => 'Social'
 
-				                ),
-           ),
-									'disable_tab' => array(
-				                    'name'    => 'disable_tab',
-				                    'label'   => __( 'Remove tab', 'dokan' ),
-				                    'desc'    => __( 'Select the tab to remove from product edit page', 'dokan' ),
-				                    'type'    => 'select',
-				                    'options' => array(
-				                    		     	     ''	        =>'--Select--',	
-							                    'edit'      => 'Edit',
-							                    'options'   =>'Options',
-							                    'inventory' => 'Inventory',
-							                    'shipping'  => 'Shipping',
-							                    'attributes'=>'Attributes',
-							                    'variations'=> 'Variations'
-							                   )
-				                    )
+				),
+			),
 
-
-           				 );
-
-                   return $settings_fields;
+		'disable_tab' => array(
+			'name'    => 'disable_tab',
+			'label'   => __( 'Remove tab', 'dokan' ),
+			'desc'    => __( 'Select the tab to remove from product edit page', 'dokan' ),
+			'type'    => 'select',
+			'options' => array(
+				''		   =>'--Select--',	
+				'options'  =>'Options',
+				'inventory' => 'Inventory',
+				'shipping'  => 'Shipping',
+				'attributes'=>'Attributes',
+				'variations' => 'Variations'
+				)
+			)
 
 
+		);
 
-	}
+	return $settings_fields;
 
-
-// Dashbaord menu removing function 
-
-	add_filter( 'dokan_get_dashboard_nav','wlr_dashbaord_nav', 11);
-
-	function wlr_dashbaord_nav($urls){
-			
-			 
-			  $menu = dokan_get_option('remove_tab','dokan_selling');
-		       unset($urls[$menu]);
-				return $urls;
-
-
-
-
-	}
-
-	add_filter( 'dokan_get_dashboard_settings_nav','wlr_dashbaord_settings_nav');
-
-		function wlr_dashbaord_settings_nav($settings_sub){
-
-				 $menu = dokan_get_option('remove_tab','dokan_selling');
-				unset($settings_sub[$menu]);
-				return $settings_sub;
-
-
-	}
-
-	add_filter( 'dokan_get_dashboard_settings_nav','wlrs_dashbaord_settings_nav',11);
-
-		function wlrs_dashbaord_settings_nav($sub_settins){
-
-	      $menu = dokan_get_option('remove_tab','dokan_selling');
-	      unset($sub_settins[$menu]);
-	      return $sub_settins;
-
-	}
-
-//Remove tab from product edit page
-
-add_filter( 'dokan_product_data_tabs','wlr_remove_tab');
-
-		function wlr_remove_tab($dokan_product_data_tabs){
-
-		          $menu = dokan_get_option('disable_tab','dokan_selling');
-		          unset($dokan_product_data_tabs[$menu]);
-		          return $dokan_product_data_tabs;
 
 
 }
 
 
- ?>
+				// Dashbaord menu removing function 
+
+add_filter( 'dokan_get_dashboard_nav','wlr_dashbaord_nav', 11);
+
+function wlr_dashbaord_nav($urls){
+
+	$menu = dokan_get_option('remove_tab','dokan_selling');
+	unset($urls[$menu]);
+	return $urls;
+
+
+
+
+}
+
+add_filter( 'dokan_get_dashboard_settings_nav','wlr_dashbaord_settings_nav');
+
+function wlr_dashbaord_settings_nav($settings_sub){
+
+	$menu = dokan_get_option('remove_tab','dokan_selling');
+	unset($settings_sub[$menu]);
+	return $settings_sub;
+
+
+}
+
+add_filter( 'dokan_get_dashboard_settings_nav','wlrs_dashbaord_settings_nav',11);
+
+function wlrs_dashbaord_settings_nav($sub_settins){
+
+	$menu = dokan_get_option('remove_tab','dokan_selling');
+	unset($sub_settins[$menu]);
+	return $sub_settins;
+
+}
+
+				//Remove tab from product edit page
+
+add_filter( 'dokan_product_data_tabs','wlr_remove_tab');
+
+function wlr_remove_tab($dokan_product_data_tabs){
+
+	$menu = dokan_get_option('disable_tab','dokan_selling');
+	unset($dokan_product_data_tabs[$menu]);
+	return $dokan_product_data_tabs;
+
+
+}
+
+?>
